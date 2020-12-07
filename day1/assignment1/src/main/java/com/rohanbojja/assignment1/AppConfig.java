65a644674abb8de4b633e52f16a3106d80699f09
Rohan Bojja <rohanbojja@Rohans-MacBook-Pro.local>
2020-12-07 09:44:55 +0530

    Small changes

--- day1/assignment1/src/main/java/com/rohanbojja/assignment1/AppConfig.java
+++ day1/assignment1/src/main/java/com/rohanbojja/assignment1/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.rohanbojja.assignment1")
+@ComponentScan("com.rohanbojja.assignment1")
 public class AppConfig {
+
 }
--- day1/assignment1/src/main/java/com/rohanbojja/assignment1/MainApp.java
+++ day1/assignment1/src/main/java/com/rohanbojja/assignment1/MainApp.java
@@ -4,11 +4,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class MainApp {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        WriterApp writerApp = applicationContext.getBean(WriterApp.class);
+        WriterApp writerApp =(WriterApp) applicationContext.getBean(WriterApp.class);
         writerApp.print("HOLO!");
     }
 }
